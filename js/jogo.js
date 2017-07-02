64e6ed69b9dcbd277a998bb2a2aa10acc13c0750
Pâmela Fidelis <fidelis.pamella@gmail.com>
2017-07-02 19:46:40 -0300

    Habilitado a marcação

--- js/jogo.js
+++ js/jogo.js
@@ -1,3 +1,6 @@
+var rodada = 1;
+var matriz_jogo = Array(3);
+
 /*function criar_cenario{
 
 	var cenario = document.createElement('table');
@@ -25,3 +28,47 @@
 
 	document.getElementById('cenario').appendChild(cenario);
 } */
+
+$(document).ready( function(){
+
+	$('#btn_iniciar').click( function(){
+
+		if($('#nome_1').val() == ''){
+			alert("Por favor informe o nome do 1º jogador");
+			return false;
+		}
+
+		if($('#nome_2').val() == ''){
+			alert("Por favor informe o nome do 2º jogador");
+			return false;
+		}
+		
+		$("#nome_jogador_1").html($('#nome_1').val());
+		$("#nome_jogador_2").html($('#nome_2').val());
+
+		//$("#pagina_inicial").hide();
+		//$("#palco_jogo").show();
+	});
+
+	$('.jogada').click( function(){
+		var id_campo = this.id;
+		jogada(id_campo);
+	});
+
+	function jogada(id_campo){
+		var icone = '';
+		var ponto = 0;
+
+		if(rodada % 2 == 1){
+			icone = 'url("imagens/marcacao_1.png")';
+			ponto = 1;
+		}else {
+			icone = 'url("imagens/marcacao_2.png")';
+			ponto = -1;
+		}
+
+		$('#'+id_campo).css('background-image', icone);
+		rodada++;
+	}
+});
+
